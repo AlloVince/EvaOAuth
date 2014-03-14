1774c07775ebc4bb6dca258df7ce6e8707c70f17
AlloVince <allo.vince@gmail.com>
2014-03-14 12:48:49 +0800

    add scope for Tencent

--- src/EvaOAuth/Adapter/OAuth2/Tencent.php
+++ src/EvaOAuth/Adapter/OAuth2/Tencent.php
@@ -4,6 +4,7 @@ namespace EvaOAuth\Adapter\OAuth2;
 
 use EvaOAuth\Adapter\OAuth2\AbstractAdapter;
 use EvaOAuth\Service\Token\Access as AccessToken;
+use ZendOAuth\OAuth;
 
 class Tencent extends AbstractAdapter
 {
@@ -11,6 +12,11 @@ class Tencent extends AbstractAdapter
     protected $authorizeUrl = "https://graph.qq.com/oauth2.0/authorize";
     protected $accessTokenUrl = "https://graph.qq.com/oauth2.0/token";
 
+    protected $defaultOptions = array(
+        'requestScheme' => OAuth::REQUEST_SCHEME_POSTBODY,
+        //'scope' => 'get_user_info,add_share,add_weibo',
+        'scope' => 'get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr',
+    );
 
     public function accessTokenToArray(AccessToken $accessToken)
     {
