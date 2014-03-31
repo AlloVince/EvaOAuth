3d623348cd2f704f3da7655eddc76c929328be58
AlloVince <allo.vince@gmail.com>
2014-03-31 15:10:48 +0800

    add tencent

--- src/EvaOAuth/Adapter/OAuth2/Tencent.php
+++ src/EvaOAuth/Adapter/OAuth2/Tencent.php
@@ -18,31 +18,56 @@ class Tencent extends AbstractAdapter
         'scope' => 'get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr',
     );
 
+    protected $openid;
+
     public function accessTokenToArray(AccessToken $accessToken)
     {
         $token = parent::accessTokenToArray($accessToken);
-        if(!isset($token['remoteUserId']) || !$token['remoteUserId']){
-            $token['remoteUserId'] = $this->getRemoteUserId();
-            $token['remoteUserName'] = $this->getRemoteUserName();
-            $token['remoteExtra'] = $this->getRawProfileString();
-        }
+        $token['remoteToken'] = $this->getRemoteToken();
+        $token['remoteUserId'] = $this->getRemoteUserId();
+        $token['remoteUserName'] = $this->getRemoteUserName();
+        $token['remoteImageUrl'] = $this->getImageUrl();
+        $token['remoteExtra'] = $this->getRawProfileString();
         return $token;
     }
 
     public function getRemoteUserId()
     {
-        $client = $this->getHttpClient();
-        $client->setUri('https://graph.qq.com/oauth2.0/me');
-        $response = $client->send();
-
-        $data = $this->parseJsonpResponse($response);
+        $data = $this->getOpenId();
         return isset($data['client_id']) ? $data['client_id'] : null;
+    }
+
+    public function getRemoteToken()
+    {
+        $data = $this->getOpenId();
+        return isset($data['openid']) ? $data['openid'] : null;
+    }
+
+    public function getImageUrl()
+    {
+        $data = $this->getRawProfile();
+        return isset($data['figureurl_2']) ? $data['figureurl_2'] : null;
     }
 
     public function getRemoteUserName()
     {
         $data = $this->getRawProfile();
         return isset($data['nickname']) ? $data['nickname'] : null;
+    }
+
+    public function getOpenId()
+    {
+        if($this->openid || false === $this->openid) {
+            return $this->openid;
+        }
+
+        $client = $this->getHttpClient();
+        $client->setUri('https://graph.qq.com/oauth2.0/me');
+        $response = $client->send();
+        if($response->getStatusCode() >= 300) {
+            return $this->openid = false;
+        }
+        return $this->openid = $this->parseJsonpResponse($response);
     }
 
     public function getRawProfile()
@@ -53,9 +78,14 @@ class Tencent extends AbstractAdapter
 
         $client = $this->getHttpClient();
         $client->setUri('https://graph.qq.com/user/get_user_info');
+        $client->setParameterGet(array(
+            'openid' => $this->getRemoteToken(),
+            'access_token' => $this->getAccessToken()->access_token,
+            'oauth_consumer_key' => $this->getConsumerKey(),
+        ));
         $response = $client->send();
         if($response->getStatusCode() >= 300) {
-            return false;
+            return $this->rawProfile = false;
         }
         return $this->rawProfile = $this->parseJsonResponse($response);
     }
